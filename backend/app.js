e79b73f6cb9a7a029261098d0822c7f566d76573
Ergavibe <arsyverenada@gmail.com>
2025-08-06 14:43:24 +0700

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,11 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+  origin: 'http://54.252.237.142', // Ganti dengan IP Publik EC2 Anda
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 // Fungsi untuk menyinkronkan database dan membuat admin pertama
 const setupDatabase = async () => {
     try {
